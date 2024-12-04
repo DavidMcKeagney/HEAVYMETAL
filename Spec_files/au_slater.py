0411f67107787feef87350818f9de886fe7ba19e
DavidMcKeagney <david.mckeagney@ucdconnect.ie>
2024-12-04 12:08:59 +0000

    New files

--- Spec_files/au_slater.py
+++ Spec_files/au_slater.py
@@ -7,14 +7,33 @@ parser=argparse.ArgumentParser()
 parser.add_argument("--slater_arg")
 parser.add_argument("--file")
 parser.add_argument("--output_file")
+parser.add_argument("--f1")
+parser.add_argument("--f2")
+parser.add_argument("--f3")
+parser.add_argument("--f4")
+parser.add_argument("--f5")
+parser.add_argument("--s_val")
 args=parser.parse_args()
+
+def slaterscaling(file,f1,f2,f3,f4,f5,slater_val):
+    if f1==1:
+        file[0]=file[0][0:50]+slater_val + file[0][52:]
+    elif f2==1:
+        file[0]=file[0][0:52] + slater_val + file[0][54:]
+    elif f3==1:
+        file[0]=file[0][0:54]+slater_val+file[0][56:]
+    elif f4==1:
+        file[0]=file[0][0:56]+slater_val+file[0][58:]
+    elif f5==1:
+        file[0]=file[0][0:58] + slater_val+file[0][60:]
+    return file[0]
 #%%
 in2=[]
 with open(args.file,'r') as text_file:
     for lines in text_file:
         in2.append(lines)
-#%%
-in2[0]=in2[0][0:54]+str(args.slater_arg)+str(args.slater_arg)+in2[0][58:]
+slaterscaling(in2, args.f1, args.f2, args.f3, args.f4, args.f5, args.s_val)
+
 #%%
 with open(args.output_file,'x') as f:
     print(f) 
---
+++ Spec_files/spectra.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 10:14:19 2024
+
+@author: padmin
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import glob
+#%%
+paths= [p.replace('\\','/') for p in glob.glob('C:/Users/padmin/Documents/au_*')]
+dele=[]
+gf=[]
+for p in paths:
+    dele.append([])
+    gf.append([])
+    with open(p) as file:
+        for f in file:
+            if len(f.split())>16:
+                if float(f.split()[11])<6:
+                    dele[-1].append(float(f.split()[11]))
+                    gf[-1].append(np.exp(float(f.split()[15])))
+#%%
+NISTDATA=np.loadtxt('C:/Users/padmin/Downloads/NIST_ASD_Output_Lines.txt',delimiter='|',dtype=str)
+Booleanindex=NISTDATA[:,4] != NISTDATA[2,4]
+SPECIFICVALUES=NISTDATA[Booleanindex]
+SPECIFICVALUES2=SPECIFICVALUES
+#%%
+p_specific=paths[30]
+spec=[]
+with open(p_specific) as file1:
+    for f1 in file1:
+        if len(f1.split())>16:
+            spec.append(f1.split())
+#%%
+spec=np.array(spec)   
+#%%
+bool1=np.logical_and(spec[:,3]=='1',spec[:,8]=='1')
+spec_6p=spec[bool1]
+bool2=np.logical_and(spec_6p[:,2]=='0.5',spec_6p[:,7]=='0.5')
+spec_6p_2=spec_6p[bool2]
+#%%
+SPECIFICVALUES_spec=[]
+for specific in SPECIFICVALUES2:
+    bool_config=np.logical_and(spec[:,3]==specific[7],spec[:,8]==specific[10])
+    temp_config=spec[bool_config]
+    j_bool=np.logical_and(temp_config[:,2]==specific[9],temp_config[:,7]==specific[12])
+    temp_j=temp_config[j_bool]
+    LS_bool=np.logical_and(temp_j[:,5]==specific[8],temp_j[:,10]==specific[11])
+    temp_LS=temp_j[LS_bool]
+    SPECIFICVALUES_spec.append(temp_LS)
+#%%
+spe_val_2=[se[0,:] for se in SPECIFICVALUES_spec]
+#%%
+dele=[float(s[11]) for s in spe_val_2]
+gf=[np.exp(float(s[15])) for s in spe_val_2]
+#%%
+dele_NIST=[float(f[6]) for f in SPECIFICVALUES2]
+gf_NIST=[np.exp(float(f[4])) for f in SPECIFICVALUES2]
+#%%
+plt.vlines(dele[2:], 0, gf[2:], color='blue', label='Theoretical') 
+plt.vlines(dele_NIST[2:], 0, gf_NIST[2:], color='red',label='Experimental') 
+plt.vlines(dele[0],0,gf[0],color='green',label='T:5d106s-5d96s6p')
+plt.vlines(dele_NIST[0],0,gf_NIST[0],color='black',label='E:5d106s-5d96s6p')
+plt.vlines(dele[1],0,gf[1],color='orange',label='T:5d96s2-5d96s6p')
+plt.vlines(dele_NIST[1],0,gf_NIST[1],color='deeppink',label='E:5d96s2-5d96s6p')
+plt.xlabel('delta_E (eV)')
+plt.ylabel('gf')
+plt.legend()
+plt.grid(True)
+              
+                    
+                    
+                    
