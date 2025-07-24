f117f14e0b222dd11bdada6aa54067918b2a3711
DavidMcKeagney <90900793+DavidMcKeagney@users.noreply.github.com>
2025-07-24 12:32:08 +0100

    Update Nested_sampling

--- Spec_files/Nested_sampling_FANO_FITTING.py
+++ Spec_files/Nested_sampling_FANO_FITTING.py
@@ -166,3 +166,81 @@ params.add('c',value=0.01,min=1e-6,max=0.02) # linewidth
 params.add('d',value=10,min=1e-6,max=3) # intensity of profile 
 params.add('e',value=-0.1,min=-0.02,max=0.02) # continuum slope
 params.add('f',value=-0.3,min=-2.4,max=2.4) # continuum constant
+#%% 
+# Old parameters for te second fano profile 3
+params=lmfit.Parameters()
+params.add('a',value=84.25,min=84.2, max=84.35) # resonance energy
+params.add('b',value=2,min=-3,max=3) # q value
+params.add('c',value=0.01,min=1e-6,max=0.3) # linewidth
+params.add('d',value=10,min=1e-6,max=3) # intensity of profile 
+params.add('e',value=-0.1,min=-0.02,max=0.02) # continuum slope
+params.add('f',value=-0.3,min=-2.4,max=2.4) # continuum constant
+#%%
+#THE GIGA FIT
+def fitfunc4(x,E1,E2,E3,q1,q2,q3,gam1,gam2,gam3,d1,d2,d3,e,f):
+    return Fano(x, E1, q1, gam1)*d1+Fano(x, E2, q2, gam2)*d2+Fano(x, E3, q3, gam3)*d3 +e*x+f
+#%%
+params=lmfit.Parameters()
+params.add('E1',value=82.5,min=82.4, max=82.6) # resonance energy1
+params.add('q1',value=0.89,min=-2,max=2) # q value1
+params.add('gam1',value=0.01,min=1e-6,max=0.7) # linewidth1
+params.add('E2',value=84.25,min=84.2, max=84.35) # resonance energy2
+params.add('q2',value=1.04,min=-3,max=3) # q value2
+params.add('gam2',value=0.01,min=1e-6,max=0.3) # linewidth2
+params.add('d1',value=0.1,min=1e-6,max=3) # intensity of profile1
+params.add('d2',value=0.1,min=1e-6,max=3) # intensity of profile2
+params.add('E3',value=80.1,min=80,max=80.2) #resonance energy3
+params.add('q3',value=3,min=-5,max=5) # q value3
+params.add('gam3',value=0.01,min=1e-6,max=1) # linewidth3
+params.add('d3',value=1,min=1e-6,max=5)# Intesity of profile3 
+params.add('e',value=-0.1,min=-0.02,max=0.02) # continuum slope
+params.add('f',value=-0.3,min=-2.4,max=2.4) # continuum constant
+#%% 
+def log_likelihood(theta):
+    params_dict = dict(zip(params.keys(), theta)) #retrieves the params dictionary as defined above
+    params1 = lmfit.Parameters()
+    for key, value in params_dict.items():
+        params1.add(key, value=value)
+        #print(params1)
+    model = fitfunc4(Energy, **params1.valuesdict()) #my function is called fit_tot. This is basically the evaluation of your function
+    residual = (model - Intensity_500ns) #/ err_ha #calculates the residuals. flux_ha is the "y" that you're fitting and err_ha are its errors
+    return -0.5 * np.sum(residual**2) #calculates the log-likelihood of the fit, evaluated at the parameters you gave. 
+
+def prior_transform(unit_cube):
+    """Map unit cube [0, 1] to parameter bounds."""
+    priors = []
+    for i, param in enumerate(params.values()):
+        lower = param.min
+        upper = param.max
+        priors.append(lower + (upper - lower) * unit_cube[i])  # Creats a Uniform prior based on the min and max values of the parameters
+        # I believe this is what you'd change to get a gaussian prior. In the flat priors, with dynesty, the attribute "value" of the params
+    # as defined in the above cell is meaningless because it samples the priors randomly. 
+    return priors
+#%%
+ndim = len(params)  # Number of parameters
+sampler = NestedSampler(loglikelihood=log_likelihood, 
+                        prior_transform=prior_transform, 
+                        ndim=len(params), 
+                        nlive=80*ndim, sample = 'rwalk')
+
+# Run the nested sampling
+
+sampler.run_nested(dlogz=0.001, print_progress=True)
+
+# Extract results
+dresults = sampler.results
+#%%
+ind = np.argmax(dresults.logl)
+sols = dresults.samples[ind]
+#%%
+plt.plot(Energy,Intensity_500ns)
+plt.plot(Energy,fitfunc4(Energy,sols[0],sols[3],sols[8],sols[1],sols[4],sols[9],sols[2],sols[5],sols[10],sols[6],sols[7],sols[11],sols[12],sols[13]))
+#%%
+weights = np.exp(dresults['logwt'] - dresults['logz'][-1])  # Compute normalized weights
+samples = dynesty.utils.resample_equal(dresults['samples'], weights)  # Resample based 
+
+parameter_names = list(params.keys())
+posterior_samples = {name: samples[:, i] for i, name in enumerate(parameter_names)}
+
+corner.corner(samples, labels=parameter_names, show_titles=True, quantiles=[0.16, 0.5, 0.84])
+plt.show()
