63e08943b154b5d5bd1b84a5a7318e172fcf3b77
DavidMcKeagney <david.mckeagney@ucdconnect.ie>
2026-01-13 14:53:18 +0000

    Updates

--- Spec_files/Cowan_Michael_Martins.py
+++ Spec_files/Cowan_Michael_Martins.py
@@ -268,6 +268,18 @@ Fano_plot3=Fano(Energy, 81.2532+1.4, 2.73, 0.26989)
 #%%
 total_cross_sections_AuI_sr=Fano_plot1+Fano_plot2+Fano_plot3
 #%%
+def bckg_500(x):
+    return -0.005*x+0.692
+def bckg_300(x):
+    return -0.011*x+1.936
+
+bck_500ns=bckg_500(Energy)
+bck_300ns=bckg_300(Energy)
+#%%
+New_Fano_plot_1_5_2_5=0.008*Fano_plot3+bck_500ns
+New_Fano_plot_2_5_3_5=0.007*Fano_plot2+bck_500ns
+New_Fano_plot_2_5_2_5=0.009*Fano_plot1+bck_500ns
+#%%
 #plt.plot(Energy, total_cross_sections_AuI_sr,label='Single Fano')
 #plt.plot(Energy, total_cross_sections_AuI,label='Michaels Calculated cross sections')
 #plt.plot(Energy,Au_I_J_1_5_2_5_int,label='J:1.5-2.5')
@@ -283,7 +295,7 @@ total_cross_sections_AuI_sr=Fano_plot1+Fano_plot2+Fano_plot3
 #plt.plot(Energy,Fano_plot1,label='J: 2.5-2.5 Fano')
 #plt.plot(Energy,Fano_plot2,label='J: 2.5-3.5 Fano')
 #plt.plot(Energy,Fano_plot3,label='J: 1.5-2.5 Fano')
-plt.plot(Energy,0.005*Au_II_J_1_2_int+np.repeat(1.1,len(Energy)),label='J:1-2')
+#plt.plot(Energy,0.005*Au_II_J_1_2_int+np.repeat(1.1,len(Energy)),label='J:1-2')
 #plt.plot(Energy,Au_II_J_2_2_int,label='J:2-2')
 #plt.plot(Energy,Fano_3_4)
 #plt.plot(Energy,Fano_2_3)
@@ -291,28 +303,32 @@ plt.plot(Energy,0.005*Au_II_J_1_2_int+np.repeat(1.1,len(Energy)),label='J:1-2')
 #plt.plot(Energy,0.009*Fano_plot1+np.repeat(0.24,len(Energy)),label='J:2.5-2.5')
 #plt.plot(Energy,0.008*Fano_plot2+np.repeat(0.28,len(Energy)),label='J:2.5-3.5')
 #plt.plot(Energy,0.01*Fano_plot3+np.repeat(0.25,len(Energy)),label='J:1.5-2.5')
-#plt.plot(Energy,1/170*Au_II_J_2_3_int+np.repeat(0.23,len(Energy)),label='J:2-3')
+#plt.plot(Energy,1/38*Au_II_J_2_3_int+np.repeat(0.95,len(Energy)),label='J:2-3')
 #plt.plot(Energy,Au_II_J_3_3_int,label='J:3-3')
-#plt.plot(Energy,1/170*Au_II_J_3_4_int+np.repeat(0.23,len(Energy)),label='J:3-4')
-#plt.plot(Energy, 1/170*(Au_II_J_2_3_int+Au_II_J_3_4_int)+np.repeat(0.23,len(Energy)),label='J:3-4+2-3')
+#plt.plot(Energy,1/38*Au_II_J_3_4_int+np.repeat(0.95,len(Energy)),label='J:3-4')
+#plt.plot(Energy, 1/38*(Au_II_J_2_3_int+Au_II_J_3_4_int)+np.repeat(0.95,len(Energy)),label='J:Summed')
 #plt.plot(Energy,Au_II_J_4_4_int,label='J:4-4')
-plt.plot(Energy,0.005*Au_II_J_4_5_int+np.repeat(1.1,len(Energy)),label='J:4-5')
+#plt.plot(Energy,0.005*Au_II_J_4_5_int+np.repeat(1.1,len(Energy)),label='J:4-5')
 #plt.plot(Energy,Au_II_J_4_5_int+Au_II_J_1_2_int,label='J: 1-2 + 4-5')
 #plt.plot(moving_avg_energy,tcs_mov_avg_AuI)
 #plt.plot(moving_avg_energy,tcs_mov_avg_AuII)
 #plt.plot(Energy,total_cross_sections_AuI,label='AuI total')
-plt.plot(Energy_300ns,Intensity_300ns,label='300ns',color='black')
+#plt.plot(Energy_300ns,Intensity_300ns,label='300ns',color='black')
 #plt.plot(Energy_300ns,15*Intensity_300ns-np.repeat(14,len(Intensity_300ns)))
-#plt.plot(Energy_500ns,Intensity_500ns,label='Exp 500ns',color='black')
+plt.plot(Energy,New_Fano_plot_1_5_2_5,label='J:1.5-2.5')
+plt.plot(Energy,New_Fano_plot_2_5_2_5,label='J:2.5-2.5')
+plt.plot(Energy,New_Fano_plot_2_5_3_5,label='J:2.5-3.5')
+plt.plot(Energy_500ns,Intensity_500ns,label='500ns',color='black')
 #plt.plot(Energy,Au_II_J_2_3_int+0.85*Au_II_J_3_4_int+0.3*Au_I_J_2_5_3_5_int)
 #plt.plot(Energy,0.8*0.4*Au_I_J_1_5_2_5_int+0.8*0.4*0.85*Au_II_J_1_2_int+0.8*0.4*0.3*Au_II_J_4_5_int+0.6*Au_II_J_2_3_int+0.6*0.85*Au_II_J_3_4_int+0.6*0.3*Au_I_J_2_5_3_5_int)
 #plt.plot(Energy,0.875*total_cross_sections_AuI_sr + 0.125*total_cross_sections_AuII,label='12.5% AuII, 87.5% AuI')
 plt.xlim(78,86)
+#plt.ylim(0.21,0.39)
 plt.legend()
 #plt.title('Au II Fano Features 300ns ')
 plt.xlabel('Energy [eV]')
-plt.ylabel('Cross section [mb]')
-#plt.ylim(0.2,0.4)
+plt.ylabel('Absorbance')
+#plt.ylim(0.91,1.69)
 #%%
 def Plotting_Fano(x,a,b):
     return a*x + b
