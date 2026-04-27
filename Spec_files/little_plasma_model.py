237d1a26481d96cd067298d00f68d1d32b485a26
DavidMcKeagney <90900793+DavidMcKeagney@users.noreply.github.com>
2026-04-27 17:23:57 +0100

    updates

--- Spec_files/little_plasma_model.py
+++ Spec_files/little_plasma_model.py
@@ -11,19 +11,53 @@ import matplotlib.pyplot as plt
 def rho(rho_0,c_0,x,t):
     return rho_0*((3/4 - x/(4*c_0*t))**3)
 
-def T(T_0,rho_0,x,t):
-    return  T_0*((rho(x,t)/rho_0)**(2/3))
+def T(T_0,rho,rho_0):
+    return  T_0*((rho/rho_0)**(2/3))
 
-def saha_factor(g_ii,g_i,x,t,X_z):
-    h=6.62607015e-34
-    m_e=9.109e-31
-    kb=1,380649e-23
-    return (2/(np.sqrt(h/(2*np.pi*m_e*kb*T(x,t)))**3))*(g_ii/g_i)*np.exp(-X_z/(kb*T(x,t)))
+def saha_factor(g_ii,g_i,T,X_z):
+    h = 6.62607015e-34       # J·s
+    m_e = 9.109e-31          # kg
+    kb = 8.617333262e-5      # eV/K
 
-def Z_bar(x,t,m_ion,g_ii,g_i,X_z):
-    rho_bar=rho(x,t)/m_ion
-    alpha_z=saha_factor(g_ii, g_i, x, t, X_z)
+    # convert k_B*T to Joules for the prefactor
+    kb_SI = 1.380649e-23     # J/K
+
+    prefactor = (2*np.pi*m_e*kb_SI*T / h**2)**(3/2)
+
+    return 2 * prefactor * (g_ii/g_i) * np.exp(-X_z/(kb*T))
+
+def Z_bar(T,rho,m_ion,g_ii,g_i,X_z):
+    rho_bar=rho/m_ion
+    alpha_z=saha_factor(g_ii, g_i,T, X_z)
     return ((-alpha_z+np.sqrt(alpha_z**2 +4*rho_bar*alpha_z))/(2*rho_bar))
 
 #%%
-    
+t=4*np.arange(0.1,3.1,0.1)
+x=np.arange(0.1,3.1,0.1)
+
+X,T_grid=np.meshgrid(x,t)
+
+rho_vals=rho(1,1,X,T_grid)
+T_vals=T(1,rho_vals,1)
+
+X_Au=9.22554*(1.6022e-19)
+g_ii_Au=1
+g_i_Au=2
+
+X_Pt=8.95883*(1.6022e-19)
+g_ii_Pt=6
+g_i_Pt=7
+
+X_Ir=8.96702*(1.6022e-19)
+g_ii_Ir=11
+g_i_Ir=10
+
+alpha_Au=saha_factor(g_ii_Au, g_i_Au, T_vals, X_Au)
+alpha_Pt=saha_factor(g_ii_Pt, g_i_Pt, T_vals, X_Pt)
+alpha_Ir=saha_factor(g_ii_Ir, g_i_Ir, T_vals, X_Ir)
+#%%
+Z_bar_Au=Z_bar(T_vals, rho_vals, 1, g_ii_Au, g_i_Au, X_Au)
+Z_bar_Pt=Z_bar(T_vals, rho_vals, 1, g_ii_Pt, g_i_Pt, X_Pt)
+Z_bar_Ir=Z_bar(T_vals, rho_vals, 1, g_ii_Ir, g_i_Ir, X_Ir)
+
+
