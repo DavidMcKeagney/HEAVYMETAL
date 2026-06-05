68b82859dfc5cc54d29403dd08974e5f554c3e4f
DavidMcKeagney <90900793+DavidMcKeagney@users.noreply.github.com>
2026-06-05 17:28:00 +0100

    Updates

--- Spec_files/numverov_method_testing.py
+++ Spec_files/numverov_method_testing.py
@@ -12,6 +12,7 @@ from numpy import linalg as LA
 def numerov(x_0,x_max,V,dx,l,Z):
     #Need to define V in another function
     #Exterior points excluded to account for boundary conditions
+    #We add the numerov corrections to the Hamiltonian manually by adding an if statement when constructing the Hamiltonian
     x_points=np.arange(x_0,x_max+dx,dx)
     x_in=x_points[1:-1]
     n=len(x_in)
@@ -30,22 +31,29 @@ def numerov(x_0,x_max,V,dx,l,Z):
         if i < n-1:
             H[i, i+1] = 1
             B[i, i+1] = 1
+        if i==1:
+            H[i,i] += -(1/12)*(Z/dx)
        
     B=1/12*B
     H=-(1/(2*(dx**2)))*H
     A=H+np.matmul(B,Wmat)
     #H_new=np.matmul(LA.inv(B),A)
     eigvalues,eigvectors=LA.eigh((A , B))
-    return x_points,x_in,eigvalues,eigvectors
+    return x_points,x_in,eigvalues,(1/np.sqrt(dx))*eigvectors
 def V(x,l,Z):
-    return Z/x + l*(l+1)/(x**2)
+    return -Z/x + l*(l+1)/(x**2)
 #%%
 sol=numerov( 0, 5, V, 0.001, 0, 0)
-#%%
+#%% TESTING NO POTENTIAL SOLUTION: IT WORKS
 x_vals=np.arange(0,5+0.001,0.001)[1:-1]
 analytical=np.sin(x_vals*(np.pi/5))
-plt.plot(sol[1],np.sqrt(1/0.001)*sol[3][0][:,0],label='numerical')
+plt.plot(sol[1],sol[3][0][:,0],label='numerical')
 plt.plot(x_vals,np.sqrt(1/np.trapz(analytical**2,x_vals,dx=0.001))*analytical,label='analytical')
 plt.legend()
 #%%
-num_sol=sol[3][0][:,0]
+s_orbit_n_1=numerov(0, 10, V, 0.001, 0, 1)
+s_n_1_analytical=np.exp(-s_orbit_n_1[1])
+#%%
+plt.plot(s_orbit_n_1[1],-s_orbit_n_1[3][0][:,0],label='numerical')
+plt.plot(s_orbit_n_1[1],2*s_orbit_n_1[1]*s_n_1_analytical,label='analytical')
+plt.legend()
