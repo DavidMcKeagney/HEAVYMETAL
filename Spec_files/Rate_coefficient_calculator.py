8654669ecafe2943a0d122198f04a1c1bd119641
DavidMcKeagney <90900793+DavidMcKeagney@users.noreply.github.com>
2026-09-08 18:29:09 +0100

    updates

---
+++ Spec_files/Rate_coefficient_calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 15:45:20 2026
+
+@author: David McKeagney
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy.integrals.quadrature import gauss_laguerre
+#%%
+#Important functions 
+def Lorentzian(x,beta,)
